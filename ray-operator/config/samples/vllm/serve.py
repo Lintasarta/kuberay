6cfe1d00132f0789d3cec6a60ce1ec0a9c645456
raihan <mraihanafiandi@gmail.com>
2025-02-02 00:58:43 +0000

    modified vllm serve to latest version of vllm

--- ray-operator/config/samples/vllm/serve.py
+++ ray-operator/config/samples/vllm/serve.py
@@ -116,4 +116,4 @@ def build_app(cli_args: Dict[str, str]) -> serve.Application:
 
 
 model = build_app(
-    {"model": os.environ['MODEL_ID'], "tensor-parallel-size": os.environ['TENSOR_PARALLELISM'], "pipeline-parallel-size": os.environ['PIPELINE_PARALLELISM']})
+    {"model": os.environ['MODEL_ID'], "tensor-parallel-size": os.environ['TENSOR_PARALLELISM'], "pipeline-parallel-size": os.environ['PIPELINE_PARALLELISM'],"trust-remote-code": True})
